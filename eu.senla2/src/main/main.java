c83b943b932d97ffe85400cd79667a369d72364a
37529 <37529@DESKTOP-L3KBGP6>
2021-01-21 18:09:02 +0300

    HomeWork2
    rainbow2

--- eu.senla2/src/main/main.java
+++ eu.senla2/src/main/main.java
@@ -14,16 +14,17 @@ public class main {
 			System.out.println("number: ");
 			int i = input.nextInt();
 			
-			int one = i / 10;
-			int ten = i % 10;
+			int index = i -1;
+			int one = (index / 10)-1;
+			int ten = index % 10;
 			
-			switch (i) {
+			switch (index) {
 			   case 0,1,2,3,4,5,6:
-				   System.out.println(rainbow1.getColour()[i]);
+				   System.out.println(rainbow1.getColour()[index]);
 			       break;     
 		      		       			   
 			   default:
-				   if(i==00 || i==11 || i==22 || i==33 || i==44 || i==55|| i==66) {
+				   if(i==77 || i==11 || i==22 || i==33 || i==44 || i==55|| i==66) {
 					   System.out.println(rainbow1.getColour()[one]);
 					   
 				   }else  if ((one >= 0 && one <7 ) && (ten >=0 && ten <7)) {
