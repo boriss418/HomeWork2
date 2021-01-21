b2c98ae8ec2972f7ce94faba8a41d690e83eb45a
37529 <37529@DESKTOP-L3KBGP6>
2021-01-21 13:26:56 +0300

    HomeWork2
    rainbow

--- eu.senla2/src/main/main.java
+++ eu.senla2/src/main/main.java
@@ -20,10 +20,13 @@ public class main {
 			switch (i) {
 			   case 0,1,2,3,4,5,6:
 				   System.out.println(rainbow1.getColour()[i]);
-			       break;
-			   
+			       break;     
+		      		       			   
 			   default:
-				   if ((one >= 0 && one <7 ) && (ten >=0 && ten <7)) {
+				   if(i==00 || i==11 || i==22 || i==33 || i==44 || i==55|| i==66) {
+					   System.out.println(rainbow1.getColour()[one]);
+					   
+				   }else  if ((one >= 0 && one <7 ) && (ten >=0 && ten <7)) {
 					   System.out.print(rainbow1.getColour2()[one]);
 					   System.out.println("-" + rainbow1.getColour()[ten]);
 				   }else
@@ -31,7 +34,6 @@ public class main {
 			    	System.out.println("probably number");
 			}
 			
-			
-			
 	}
 }
+		
