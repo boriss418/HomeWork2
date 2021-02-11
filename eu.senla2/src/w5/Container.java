436fb0df4a7f80c18a39cd369bde4dc30ad23236
37529 <37529@DESKTOP-L3KBGP6>
2021-02-11 18:57:16 +0300

    HomeWork5
    not complete

---
+++ eu.senla2/src/w5/Container.java
@@ -0,0 +1,22 @@
+package w5;
+
+public class Container  {
+	
+	
+	
+	int d = 10;
+	 int h = 10 + (int) (Math.random()*100);
+	
+	 double cylinder = (d * d / 2) * h;
+	 double square = ((d * d)*3.14 / 4) * h;
+	 double cone = ((d * d)*3.14 / 4) *( h/3);
+	
+	
+	
+	 int D = 20;
+	
+	 double bigcylinder = (D * D / 2) * h;
+	 double bigsquare = ((D * D)*3.14 / 4) * h;	 
+	 double bigcone = ((D * D)*3.14 / 4) *( h/3);
+
+}
---
+++ eu.senla2/src/w5/Main.java
@@ -0,0 +1,17 @@
+package w5;
+
+import hm5.Volum;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Container container = new Container();
+		Deck deck = new Deck();
+		
+		//deck.getDeck();
+		
+		System.out.println(container.square);
+		
+		
+	}
+}
